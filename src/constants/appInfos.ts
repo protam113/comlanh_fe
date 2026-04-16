0504d00abd8ad270d3eeee577a2d7a459b87365d
protam113 <hoangpm2003.strix@gmail.com>
2026-04-16 14:51:23 +0700

    feat: add use client directive to components for Next.js build

--- src/constants/appInfos.ts
+++ src/constants/appInfos.ts
@@ -8,8 +8,9 @@ export const appInfo = {
   description:
     process.env.NEXT_PUBLIC_SITE_DESCRIPTION ||
     'Cơm Lành - Đối tác tin cậy của hàng nghìn quán ăn. Cung cấp gạo chất lượng cao với quy trình "Thử Thật - Nấu Thật". Gạo sạch, an toàn, phù hợp cho kinh doanh.',
-  domain: process.env.NEXT_PUBLIC_SITE_DOMAIN || 'https://gaolanh.com',
-  ogImage: process.env.NEXT_PUBLIC_OG_IMAGE || '/imgs/vsv.webp',
+  domain:
+    process.env.NEXT_PUBLIC_SITE_DOMAIN || 'https://comlanh-fe.vercel.app/',
+  ogImage: process.env.NEXT_PUBLIC_OG_IMAGE || '/imgs/ogImage.png',
   themeColor: process.env.NEXT_PUBLIC_THEME_COLOR || '#ffffff',
   keywords: process.env.NEXT_PUBLIC_SITE_KEYWORDS?.split(',').map((k) =>
     k.trim()
